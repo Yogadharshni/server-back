c60e3e3391375c4114352fe5a417dafd84c5d9ef
Yogadharshni <yogdharshni06@gmail.com>
2023-08-02 16:56:59 +0530

    updated

--- index.js
+++ index.js
@@ -12,38 +12,12 @@ connectDB();
 const app = express();
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors({
-  origin: "http://localhost:5000"
-}));
+app.use(cors());
 
 
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-  return await fn(req, res)
-}
-
-const handler = (req, res) => {
-  const d = new Date()
-  res.end(d.toString())
-}
-
-module.exports = allowCors(handler)
-
-// app.use('/', (req, res) => {
-//   res.send("This is a stack overflow clone API")
-// })
+app.use('/', (req, res) => {
+  res.send("This is a stack overflow clone API")
+})
 
 app.use("/user", userRoutes);
 app.use("/questions", questionRoutes);
